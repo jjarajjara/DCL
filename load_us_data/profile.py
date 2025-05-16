19330ad9cfb70b52f58745eb3c755fa46bb60c4a
jjarajjara <jjarajjara1903@naver.com>
2025-05-16 11:04:30 +0900

    Code Changed

--- load_us_data/profile.py
+++ load_us_data/profile.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2f9f45944e9a8d40abde7f74681dfbb8f5f867fc673e5738dd02030e2665aa0
-size 12046
+oid sha256:f36c10935929d31605b234840bf6a7808670536d2e7fddbc17d0c4262977bf27
+size 12029
